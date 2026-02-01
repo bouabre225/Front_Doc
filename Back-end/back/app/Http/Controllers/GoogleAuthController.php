7e3bb28446fe27d838f6cd30bdb3a3fdb7fae056
bouabre225 <angebouabre463@gmail.com>
2026-02-01 22:22:13 +0100

    feat: add google provider controller

---
+++ Back-end/back/app/Http/Controllers/GoogleAuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\Auth\GoogleAuthservice;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    /**
+     * Redirect to Google for authentication
+     */
+    public function redirect()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    /**
+     * Handle Google callback
+     * @param GoogleAuthService $service
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function callback(GoogleAuthService $service)
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+        return response()->json($service->handleGoogleUser($googleUser));
+    }
+}
