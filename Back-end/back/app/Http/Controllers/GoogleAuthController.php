b11e6435dc421a5a876f33ba45f3f5623ec2eced
bouabre225 <angebouabre463@gmail.com>
2026-02-04 22:08:21 +0100

    fix: redirect to app.front

--- Back-end/back/app/Http/Controllers/GoogleAuthController.php
+++ Back-end/back/app/Http/Controllers/GoogleAuthController.php
@@ -24,6 +24,6 @@ class GoogleAuthController extends Controller
     public function callback(GoogleAuthService $service)
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
-        return response()->json($service->handleGoogleUser($googleUser));
+        return redirect()->away(config('app.frontend_url')."/auth/callback?token=".$token);
     }
 }
