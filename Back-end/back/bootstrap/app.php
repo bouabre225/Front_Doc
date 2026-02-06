31a00d8cc30528385b8e9665b7fcc6dcf6800b93
bouabre225 <angebouabre463@gmail.com>
2026-02-06 23:26:25 +0100

    fix: add middleware in bootstrap

--- Back-end/back/bootstrap/app.php
+++ Back-end/back/bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'token_ability' => \App\Http\Middleware\TokenAbilityMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
