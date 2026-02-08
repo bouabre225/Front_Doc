da962b77bd69706ed2524abcf5c69ae9db69f699
bouabre225 <angebouabre463@gmail.com>
2026-02-08 15:10:16 +0100

    fix: import des biblioteques

--- Back-end/back/app/Services/Auth/AuthService.php
+++ Back-end/back/app/Services/Auth/AuthService.php
@@ -4,6 +4,9 @@ namespace App\Services\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 
 class AuthService
 {
