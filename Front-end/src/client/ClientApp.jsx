40872e0a76e073436552eb3fe1263fc69d1d9f3b
bouabre225 <angebouabre463@gmail.com>
2026-03-09 14:43:24 +0100

    fix: ajout des routes de mot de passe oublie

--- Front-end/src/client/ClientApp.jsx
+++ Front-end/src/client/ClientApp.jsx
@@ -9,6 +9,7 @@ import Profile from './pages/auth/Profile';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import ForgotPassword from './pages/auth/ForgotPassword';
+import ResetPassword from './pages/auth/ResetPassword';
 import PublishEquipement from './pages/sellers/PublishEquipement';
 import Notifications from './components/notifications/Notifications';
 import Cart from './components/cart/Cart';
@@ -28,6 +29,7 @@ function ClientApp() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
           <Route path='/publish-equipment' element={<PublishEquipement />} />
           <Route path='/notifications' element={<Notifications />} />
         </Routes>
